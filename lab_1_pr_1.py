1d0e6b48c0fc2bcd2bec76983e81dafa92a8b6d9
gulluiza <66479414+gulluiza@users.noreply.github.com>
2020-06-11 12:51:44 +0200

    Прикрепляю файлы с заданиями 1-4, 5 не получается

---
+++ lab_1_pr_1.py
@@ -0,0 +1,11 @@
+s = 'Hello, World!'
+print(s)
+
+s = 'Маша + Петя = Любовь'
+print(s)
+
+x = ('3 + 4')
+print(x)
+
+x = (3 + 4)
+print(x)
---
+++ lab_1_pr_2.py
@@ -0,0 +1,11 @@
+x = 'Hello, World!'
+print(type(x))
+
+y = 3 + 4
+print(type(y))
+
+a = 3 / 4
+print(type(a))
+
+b = [1, 2, 5, 10, 100]
+print(type(b))
