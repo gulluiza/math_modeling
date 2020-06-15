cad9eafecf81a63a2205ebe756e163ca8fc8939a
gulluiza <66479414+gulluiza@users.noreply.github.com>
2020-06-15 14:08:45 +0200

    Update lab_2_dop_5.py

--- lab_2_dop_5.py
+++ lab_2_dop_5.py
@@ -1,1 +1,16 @@
-x = int(input('введите число '))y = 2a = '1'b = '*'while x != 1:    if x % y != 0:        y = y + 1    elif x % y == 0:        a = a + b + str(y)        x = x // y    else:        print('-')        print('простые множители вашего числа и единичка:', a)
+x = int(input('введите число '))
+
+y = 2
+a = ' '
+b = '*'
+
+while x != 1:
+    if x % y != 0:
+        y = y + 1
+    elif x % y == 0:
+        a = a + b + str(y)
+        x = x // y
+    else:
+        print('-')
+        
+print('простые множители вашего числа:', a)
