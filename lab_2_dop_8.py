3883a23ba24ef5d7d158cc979873a10bdaeb3962
gulluiza <66479414+gulluiza@users.noreply.github.com>
2020-06-15 13:24:31 +0200

    Add files via upload

---
+++ lab_2_dop_8.py
@@ -0,0 +1,42 @@
+for i in range(1, 10, 1):
+    print(i, end = '   ')
+
+print(' ')
+    
+for i in range(2, 20, 2):
+    print(i, end = '   ')
+    
+print(' ') 
+
+for i in range(3, 30, 3):
+    print(i, end = '   ')
+
+print(' ') 
+
+for i in range(4, 40, 4):
+    print(i, end = '   ')
+
+print(' ') 
+
+for i in range(5, 50, 5):
+    print(i, end = '   ')
+   
+print(' ') 
+
+for i in range(6, 60, 6):
+    print(i, end = '   ')
+    
+print(' ') 
+
+for i in range(7, 70, 7):
+    print(i, end = '   ')
+    
+print(' ') 
+
+for i in range(8, 80, 8):
+    print(i, end = '   ')
+    
+print(' ') 
+
+for i in range(9, 90, 9):
+    print(i, end = '   ')
