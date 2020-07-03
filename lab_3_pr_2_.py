df6798bc5e775d3aa79720efa58bb49142f0e101
gulluiza <66479414+gulluiza@users.noreply.github.com>
2020-07-03 13:46:27 +0200

    вариант другого примера для второго задания

--- lab_3_pr_2_.py
+++ lab_3_pr_2_.py
@@ -4,8 +4,8 @@ from math import cos, sin, pi
 from astro_constant import g
 
 v = float(input('введите начальную скорость в м/с: '))
-n = float(input('введите угол в формате pi*n, n = '))
-a = pi * n
+n = float(input('введите угол в градусах:'))
+a = (n * pi) / 180
 t = float(input('введите момент времени в секундах: '))
 
 r = ((((v * cos(a))**2) + (v * sin(a) - g * t)**2)**(1.5)) / (v * g * cos(a))
